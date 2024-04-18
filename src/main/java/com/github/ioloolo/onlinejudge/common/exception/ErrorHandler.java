99a9550354a2392a5e539fa72bc4a77aa9e96e02
Son Daehyeon <sondaehyeon01@gmail.com>
2024-04-18 12:08:24 +0900

    fix: Error Handler

--- src/main/java/com/github/ioloolo/onlinejudge/common/exception/ErrorHandler.java
+++ src/main/java/com/github/ioloolo/onlinejudge/common/exception/ErrorHandler.java
@@ -1,15 +1,25 @@
 package com.github.ioloolo.onlinejudge.common.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ErrorHandler {
 
-	@ExceptionHandler(Exception.class)
+	@ExceptionHandler(Throwable.class)
 	public ResponseEntity<ErrorResponse> handleException(Exception e) {
 
 		return ResponseEntity.internalServerError().body(new ErrorResponse(e));
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleValidatedException(MethodArgumentNotValidException e) {
+
+		FieldError error = e.getFieldError();
+
+		return handleException(error == null ? e : new Exception(error.getDefaultMessage()));
 	}
 }
